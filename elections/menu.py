d6ddff65c4e4a43520ad896c89b1f1ed63fb0629
Davi César <davicesarmorais@gmail.com>
2024-09-15 12:04:27 -0300

    refactor: variable names, documentation and menu operation

--- elections/menu.py
+++ elections/menu.py
@@ -1,75 +1,114 @@
 from elections.elec_data import candidate_by_code, candidates_by_city_and_role
 from elections.view import gen_statistics_html, open_file
+import os, time
 
 def main():
   while True:
     display_menu()
     
-    res = input("")
-    
-    if res == "1":
-      option_1()
+    user_input = input("> ")
+    match user_input:
       
-    elif res == "2":
-      option_2()    
+      case "1":
+        option_by_city_and_role()
+
+      case "2":
+        option_by_code()    
+
+      case "3":
+        option_gen_statistics()          
+
+      case "0":
+        print("Encerrando...")
+        break
       
-    elif res == "3":
-      option_3()          
+      case _:
+        print("Opção inválida, tente de novo...")
+        time.sleep(0.8)
+        continue
       
-    elif res == "0":
-      print("Encerrando...")
-      break
-    else:
-      clear()
-      print("Opção inválida, tente de novo...")
-      
-    if not proceed(): break
       
 def clear():
-  import os
-  os.system('cls' if os.name == 'nt' else 'clear')
-      
-def proceed() -> bool:
-  cont = input("Continuar (S ou N)? ").strip().upper()
-  if cont == "N": 
+  os.system('clear||cls')
+  
+def validate_number(user_input: str) -> bool:
+  """
+  Valida se um input do usuário é um número.
+  """
+  if not user_input.isdecimal():
+    print("Digite um número!")
+    time.sleep(0.8)
     return False
-  elif cont not in "SN":
-    print("Resposta inválida!")
-    proceed()
+  
   return True
+
 
 def display_menu():
   clear()
   print(
-    '''
-  Bem vindo(a) ao ElectionsAPE\n
-  Opções:
-  1 - Listar candidato por município e cargo
-  2 - Exibir detalhes de um candidato a partir do código
-  3 - Gerar página com estatísticas interessantes sobre os candidatos
-  0 - Fechar aplicação\n
-  O que você deseja fazer?''', end=" "
+'''Bem vindo(a) ao ElectionsAPE\n
+1 - Listar candidato por município e cargo
+2 - Exibir detalhes de um candidato a partir do código
+3 - Gerar página com estatísticas interessantes sobre os candidatos
+0 - Fechar aplicação\n
+O que você deseja fazer?'''
   )
   
-def option_1():
-  clear()
-  cityCode = int(input("Código numérico da cidade desejada: "))
-  roleCode = int(input("Código numérico do cargo desejado: "))
-  print(candidates_by_city_and_role(cityCode, roleCode).to_string(index=False))
 
-def option_2():
+def option_by_city_and_role() -> None:
+  """
+  Opção do menu que lista os candidatos por município e cargo.
+  """
+  
   clear()
-  code = int(input("Código sequencial numérico do candidato desejado: "))
-  print(candidate_by_code(code).to_string(index=False))
+  print("Digite 'q' para voltar ao menu anterior.")
+  
+  city_code = input("Código numérico da cidade desejada: ")
+  if city_code in "qQ":
+    return
+  
+  if not validate_number(city_code):
+    return option_by_city_and_role()
+    
+  role_code = input("Código numérico do cargo desejado: ")
+  if role_code in "qQ": 
+    return
+  
+  if not validate_number(role_code):
+    return option_by_city_and_role()
+  
+  print(candidates_by_city_and_role(city_code, role_code).to_string(index=False))
 
-def option_3():
+
+def option_by_code() -> None:
+  """
+  Opção do menu que exibe detalhes de um candidato a partir do código.
+  """
   clear()
-  res = input("Deseja abrir a página com as estatísticas (S ou N)? ").upper()
-  if res == "S":
+  print("Digite 'q' para voltar ao menu anterior.")
+  
+  candidate_code = input("Código sequencial numérico do candidato desejado: ")
+  if candidate_code in "qQ":
+    return
+  
+  if not validate_number(candidate_code):
+    return option_by_code()
+  
+  print(candidate_by_code(candidate_code).to_string(index=False))
+
+
+def option_gen_statistics() -> None:
+  """
+  Opção do menu que gera uma página HTML com estatísticas sobre os candidatos.
+  """
+  clear()
+  user_confirm = input("Deseja abrir a página com as estatísticas (S ou N)? ").upper()
+  if user_confirm == "S":
     open_file(gen_statistics_html())    
-  elif res not in "SN":
+  elif user_confirm not in "SN":
     print("Opção inválida...")
-    option_3()
+    time.sleep(0.8)
+    option_gen_statistics()
     
 
 if __name__ == "__main__":
