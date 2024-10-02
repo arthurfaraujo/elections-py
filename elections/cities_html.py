f906ad057a545ad97f29d386c3f7ed03bbd26e85
Davi César <davicesarmorais@gmail.com>
2024-10-02 00:25:12 -0300

    feat:finish tailwind on all flask pages

--- elections/cities_html.py
+++ elections/cities_html.py
@@ -16,7 +16,7 @@ cities_df = cities()
 def index():
     lista_municipios = "<ul class='flex flex-wrap gap-2'>"
     for municipio in cities_df:
-        lista_municipios += f"<li class='flex grow'><a class='justify-center p-4 gap-0 hover:scale-105 rounded border duration-300 text-center flex w-full h-full' href='/municipio?municipio={municipio}'>{municipio}</a></li>"
+        lista_municipios += f"<li class='flex grow'><a class='justify-center p-2 gap-0 hover:scale-105 rounded border duration-300 text-center flex w-full h-full' href='/municipio?municipio={municipio}'>{municipio.title()}</a></li>"
     lista_municipios += "</ul>"
     return render_template_string("""
 <html>
@@ -44,33 +44,51 @@ def municipio():
         
         prefeitos = ""
         for prefeito in candidates_city['prefeitos']:
-            prefeitos += f"<li><a href='/candidato?id={prefeito[1]}&cargo=Prefeito&municipio={municipio}'>{prefeito[0]}</a></li>"
+            prefeitos += f"<li><a class='flex w-full p-1 px-2 hover:bg-gray-200 border duration-300 text-center' href='/candidato?id={prefeito[1]}&cargo=Prefeito&municipio={municipio}'>{prefeito[0].title()}</a></li>"
         
         vice_prefeitos = ""
         for vice_prefeito in candidates_city['vice_prefeitos']:
-            vice_prefeitos += f"<li><a href='/candidato?id={vice_prefeito[1]}&cargo=Vice-Prefeito&municipio={municipio}'>{vice_prefeito[0]}</a></li>"
+            vice_prefeitos += f"<li><a class='flex w-full p-1 px-2 hover:bg-gray-200 border duration-300 text-center' href='/candidato?id={vice_prefeito[1]}&cargo=Vice-Prefeito&municipio={municipio}'>{vice_prefeito[0].title()}</a></li>"
         
         
         vereadores = ""
         for vereador in candidates_city['vereadores']:
-            vereadores += f"<li><a href='/candidato?id={vereador[1]}&cargo=Vereador&municipio={municipio}'>{vereador[0]}</a></li>"
+            vereadores += f"<li><a class='flex w-full p-1 px-2 hover:bg-gray-200 border duration-300 text-center' href='/candidato?id={vereador[1]}&cargo=Vereador&municipio={municipio}'>{vereador[0].title()}</a></li>"
         
         
         return render_template_string("""
-        <html>
-        <head><title>{{ nome_municipio }}</title></head>
-        <body>
-            <h1>{{ nome_municipio }}</h1>
-            <a href="/">Voltar</a>
-            <h2>Prefeito:</h2>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Estatísticas Eleitorais - Eleições 2024</title>
+</head>
+
+<body class="container mx-auto flex flex-col">
+    <div class='flex flex-col items-center sticky py-4 top-0 backdrop-blur border-b'>
+        <h1 class="font-bold text-gray-700 text-2xl text-center">{{ nome_municipio }}</h1>
+        <a class="bg-white hover:bg-gray-200 duration-300 mt-4 mx-auto w-fit px-8 py-1 text-gray-700 text-center rounded border" href="/">Voltar para página inicial</a>
+    </div>
+    <div class="mt-4 flex flex-col mx-auto">
+        <div class="border p-4 rounded m-4">
+            <h2 class="text-gray-600 text-center text-xl font-bold mb-4 pb-4 border-b">Prefeito:</h2>
             <ul>{{ prefeito | safe }}</ul>
-            <h2>Vice-Prefeito:</h2>
+        </div>
+
+        <div class="border p-4 rounded m-4">
+            <h2 class="text-gray-600 text-center text-xl font-bold mb-4 pb-4 border-b">Vice-Prefeito:</h2>
             <ul>{{ vice_prefeito | safe }}</ul>
-            <h2>Vereadores:</h2>
+        </div>
+
+        <div class="border p-4 rounded m-4">
+            <h2 class="text-gray-600 text-center text-xl font-bold mb-4 pb-4 border-b">Vereadores:</h2>
             <ul>{{ vereadores | safe }}</ul>
-        </body>
-        </html>
-        """, nome_municipio=municipio, prefeito=prefeitos, vice_prefeito=vice_prefeitos, vereadores=vereadores)
+        </div>
+    </div>
+</body>
+</html>
+        """, nome_municipio=municipio.title(), prefeito=prefeitos, vice_prefeito=vice_prefeitos, vereadores=vereadores)
 
 
 @app.route('/candidato')
@@ -87,26 +105,43 @@ def candidato():
     Partido = candidato_df['Partido'].values[0]
     SiglaPartido = candidato_df['SG_PARTIDO'].values[0]
     redes = ""
+    
     # arthur nao apaga <-------------------------
     # variavel redes terá varias <li> com links
     for index, row in link_redes.iterrows():
         # index, row para iterar por cada indice e linha
-        redes += f"<li><a href='{row['Link da rede social']}'>{row['Link da rede social']}</a></li>"
+        redes += f"<li><a class='flex w-full p-1 px-2 hover:bg-gray-200 border duration-300' href='{row['Link da rede social']}'>{row['Link da rede social']}</a></li>"
         #row['Link da rede social'] para percorrer justamente por essa coluna
 
     return render_template_string("""
-    <html>
-    <head><title>{{ NomeCandidato }}</title></head>
-    <body>
-        <h1> ({{ NumeroCandidato }}) {{ NomeCandidato }} - {{ cargo }}</h1>
-        <p>Candidato a {{ cargo }} no município de {{ municipio }}.</p>
-        <p>{{ NomeUrna }} | {{ Partido }} ({{ SiglaPartido }})</p>
-        <img src="{{ imagem }}"> <br>
-        <ul>{{ link_redes | safe }}</ul>
-        <a href="/municipio?municipio={{ municipio }}">Voltar para o município</a>
-    </body>
-    </html>
-    """, link_redes=redes, imagem=imagem, id_candidato=id_candidato, cargo=cargo, municipio=municipio, NomeCandidato=NomeCandidato, NomeUrna=NomeUrna, NumeroCandidato=NumeroCandidato, Partido=Partido, SiglaPartido=SiglaPartido)
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>{{ NomeCandidato }}</title>
+</head>
+<body class="container mx-auto flex flex-col">
+    <div class='flex flex-col items-center sticky py-4 top-0 backdrop-blur border-b'>
+        <h1 class="font-bold text-gray-700 text-2xl text-center">({{ NumeroCandidato }}) {{ NomeCandidato }} - {{ cargo }}</h1>
+        <a class="bg-white hover:bg-gray-200 duration-300 mt-4 mx-auto w-fit px-8 py-1 text-gray-700 text-center rounded border" href="/municipio?municipio={{ municipio }}">Voltar para o município</a>
+    </div>
+    
+    
+    <div class="flex gap-12 justify-center mt-8">
+        <img class="hidden sm:block rounded w-48 h-48 object-cover" src="{{ imagem }}">
+        <div class="text-center sm:text-left">
+            <h2 class="font-bold text-gray-600 text-xl">{{ NomeUrna }} | {{ Partido }} ({{ SiglaPartido }})</h2>
+            <p class="font-semibold text-gray-500 mt-2 mb-4">Candidato a {{ cargo }} no município de {{ municipio }}.</p>
+            <ul class="flex flex-col border p-4 rounded">
+                <h3 class="text-gray-600 font-bold text-lg border-b pb-2 mb-4">Redes sociais</h3>
+                {{ link_redes | safe }}
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
+    """, link_redes=redes, imagem=imagem, id_candidato=id_candidato, cargo=cargo, municipio=municipio, NomeCandidato=NomeCandidato.title(), NomeUrna=NomeUrna.title(), NumeroCandidato=NumeroCandidato, Partido=Partido.title(), SiglaPartido=SiglaPartido)
 
 
 def start_flask():
